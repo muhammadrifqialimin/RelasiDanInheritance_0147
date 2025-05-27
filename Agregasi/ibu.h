af96e75d177a710ecb3d8add396144e2e8c1e6a1
Muhammad Rifqi Alimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-05-27 18:24:04 +0700

    membuat agregasi Ibu

---
+++ Agregasi/ibu.h
@@ -0,0 +1,31 @@
+#ifndef IBU_H
+#define IBU_H
+
+class Ibu {
+    public:
+        string nama;
+        vector<anak*> daftar_anak;
+
+        ibu(string pNama): nama(pNama){
+            cout << "Ibu \"" << nama << "\" ada\n";
+        }
+
+        ~ibu() {
+            cout << "Ibu \"" << nama << "\" tidak ada\n"
+        }
+        void tambahAnak(anak*);
+        void cetakAnak();
+};
+
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+void ibu::cetakAnak() {
+    cout << "Daftar Anak Dari Ibu \"" << this->nama << "\":\n";
+    for (int i = 0; i < daftar_anak.size(): i++){
+        cout << daftar_anak(i)->nama << endl;
+    }
+    cout << endl;
+}
+
+#endif
